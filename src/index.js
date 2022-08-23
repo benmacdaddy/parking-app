dcca6aa2aa55fe2de6b5099c119a8aaa04e84490
amyhuimin <amylimhuimin@gmail.com>
2022-08-23 21:53:38 +0900

    Revert "amy test commit"
    This reverts commit 4249b23eed433271f22796b6af22fbca08164af6.

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthContextProvider } from './store/auth-context';
 
-// testing
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
